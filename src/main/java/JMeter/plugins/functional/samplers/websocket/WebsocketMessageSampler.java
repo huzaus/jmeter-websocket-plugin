f0b2bd66ad9d6c4cc2df17bae9e8c202dee3fab3
huzaus <siarhei.huzau@gmail.com>
2017-01-17 11:19:08 +0100

    Add CookieManager and HeaderManager support

--- src/main/java/JMeter/plugins/functional/samplers/websocket/WebsocketMessageSampler.java
+++ src/main/java/JMeter/plugins/functional/samplers/websocket/WebsocketMessageSampler.java
@@ -1,8 +1,14 @@
 package JMeter.plugins.functional.samplers.websocket;
 
+import com.google.common.base.Function;
+import com.google.common.base.Optional;
+import org.apache.jmeter.protocol.http.control.CookieManager;
+import org.apache.jmeter.protocol.http.control.HeaderManager;
 import org.apache.jmeter.samplers.AbstractSampler;
 import org.apache.jmeter.samplers.Entry;
 import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jmeter.testelement.property.TestElementProperty;
 
 public class WebsocketMessageSampler extends AbstractSampler {
 
@@ -13,6 +19,8 @@ public class WebsocketMessageSampler extends AbstractSampler {
     private static final String RESPONSE_TIME_OUT = "responseTimeOut";
     private static final String PATH = "path";
     private static final String MESSAGE = "message";
+    private static final String COOKIE_MANAGER = "cookie_manager";
+    private static final String HEADER_MANAGER = "header_manager";
 
     public WebsocketMessageSampler() {
         setName("Websocket Message Sampler");
@@ -21,6 +29,49 @@ public class WebsocketMessageSampler extends AbstractSampler {
     @Override
     public SampleResult sample(Entry entry) {
         return new SampleResult();
+    }
+
+    @Override
+    public void addTestElement(TestElement el) {
+        if (el instanceof CookieManager) {
+            setCookieManager((CookieManager) el);
+        } else if (el instanceof HeaderManager) {
+            setHeaderManager((HeaderManager) el);
+        } else {
+            super.addTestElement(el);
+        }
+    }
+
+    public CookieManager getCookieManager() {
+        return Optional.fromNullable(getProperty(COOKIE_MANAGER).getObjectValue())
+                .transform(
+                        new Function<Object, CookieManager>() {
+                            @Override
+                            public CookieManager apply(Object property) {
+                                return (CookieManager) property;
+                            }
+                        })
+                .orNull();
+    }
+
+    public void setCookieManager(CookieManager cookieManager) {
+        setProperty(new TestElementProperty(COOKIE_MANAGER, cookieManager));
+    }
+
+    public HeaderManager getHeaderManager() {
+        return Optional.fromNullable(getProperty(HEADER_MANAGER).getObjectValue())
+                .transform(
+                        new Function<Object, HeaderManager>() {
+                            @Override
+                            public HeaderManager apply(Object property) {
+                                return (HeaderManager) property;
+                            }
+                        })
+                .orNull();
+    }
+
+    public void setHeaderManager(HeaderManager headerManager) {
+        setProperty(new TestElementProperty(HEADER_MANAGER, headerManager));
     }
 
     public String getServerNameOrIp() {
--- src/test/groovy/JMeter/plugins/functional/samplers/websocket/WebsocketMessageSamplerSpec.groovy
+++ src/test/groovy/JMeter/plugins/functional/samplers/websocket/WebsocketMessageSamplerSpec.groovy
@@ -1,10 +1,14 @@
 package JMeter.plugins.functional.samplers.websocket
 
+import org.apache.jmeter.protocol.http.control.CookieManager
+import org.apache.jmeter.protocol.http.control.HeaderManager
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Unroll
 
 import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.CONNECT_TIME_OUT
+import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.COOKIE_MANAGER
+import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.HEADER_MANAGER
 import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.MESSAGE
 import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.PATH
 import static JMeter.plugins.functional.samplers.websocket.WebsocketMessageSampler.PORT_NUMBER
@@ -18,7 +22,7 @@ class WebsocketMessageSamplerSpec extends Specification {
     @Subject
     WebsocketMessageSampler sampler = new WebsocketMessageSampler()
 
-    def "Should set #field as getPropertyAs#type(#property) "() {
+    def "#field field value should be stored in #property property of #type type"() {
         when:
         sampler."$field" = value
         then:
@@ -32,5 +36,48 @@ class WebsocketMessageSamplerSpec extends Specification {
         'responseTimeOut' | RESPONSE_TIME_OUT | 'String' | '5000'
         'path'            | PATH              | 'String' | '/websocket'
         'message'         | MESSAGE           | 'String' | 'CONNECT\\naccept-version:1.1,1.0\\nheart-beat:10000,10000\\n\\n\\u0000'
+    }
+
+    def "Should not set #property property via addTestElement method when class type is not matched"() {
+        when:
+        sampler.addTestElement(manager)
+        then:
+        sampler.getProperty(property).objectValue == null
+        where:
+        property       | manager
+        COOKIE_MANAGER | new HeaderManager()
+        HEADER_MANAGER | new CookieManager()
+    }
+
+    def "Should set #property property via addTestElement method when class type is matched"() {
+        when:
+        sampler.addTestElement(manager)
+        then:
+        sampler.getProperty(property).objectValue == manager
+        where:
+        property       | manager
+        COOKIE_MANAGER | new CookieManager()
+        HEADER_MANAGER | new HeaderManager()
+    }
+
+    def "Should return null when #property is not set"() {
+        expect:
+        sampler."$property" == null
+        where:
+        property << ["cookieManager", "headerManager"]
+
+    }
+
+    def "Should return value when #property is set"() {
+        when:
+        sampler."$property" = value
+        then:
+        sampler."$property" == value
+        where:
+        property        | value
+        "cookieManager" | new CookieManager()
+        "cookieManager" | null
+        "headerManager" | new HeaderManager()
+        "headerManager" | null
     }
 }
