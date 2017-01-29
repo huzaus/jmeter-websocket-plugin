b0a68dc96e96e4d7519a70ba2a8eb63f000c4d25
huzaus <siarhei.huzau@gmail.com>
2017-01-29 14:28:01 +0100

    Fix jmeter.properties location

--- gradleSetup.sh
+++ gradleSetup.sh
@@ -2,10 +2,12 @@
 GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
 export GRADLE_PROPERTIES
 
-#if [ ! -f "$GRADLE_PROPERTIES" ]; then
+if [ ! -f "$GRADLE_PROPERTIES" ]; then
      echo "Creating Gradle Properties file..."
      touch $GRADLE_PROPERTIES
 
      echo "Writing jmeterHome to gradle.properties..."
      echo "jmeterHome=$JMETER_HOME" >> $GRADLE_PROPERTIES
-#fi
+     echo "Writing jmeterProperties to gradle.properties..."
+     echo "jmeterProperties=$JMETER_HOME/bin/jmeter.properties" >> $GRADLE_PROPERTIES
+fi
