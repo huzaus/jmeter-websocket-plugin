09dd316a4ffef3cd64d1a25a8c8ee9c507ab5045
huzaus <siarhei.huzau@gmail.com>
2017-02-03 11:42:00 +0100

    Rename *GuiGuiSpec -> *GuiSpec

--- src/test/groovy/com/jmeter/websocket/plugin/elements/samplers/WebsocketSessionSamplerGuiSpec.groovy
+++ src/test/groovy/com/jmeter/websocket/plugin/elements/samplers/WebsocketSessionSamplerGuiSpec.groovy
@@ -6,19 +6,24 @@ import spock.lang.Subject
 import spock.lang.Unroll
 
 @Unroll
-class WebsocketMessageSamplerGuiGuiSpec extends JmeterAbstractGuiSpec {
+class WebsocketSessionSamplerGuiSpec extends JmeterAbstractGuiSpec {
+
     @Subject
-    WebsocketMessageSamplerGui samplerGui = new WebsocketMessageSamplerGui()
-    
+    WebsocketSessionSamplerGui samplerGui = new WebsocketSessionSamplerGui()
+
     def "#component should be initialized in constructor with #label"() {
         expect:
             samplerGui."$component".mLabel.text == label
         where:
-            component   || label
-            'sessionId' || 'Session id:'
-            'message'   || 'Message:'
+            component        || label
+            'serverNameOrIp' || 'Server Name or IP:'
+            'portNumber'     || 'Port Number:'
+            'protocol'       || 'Protocol:'
+            'connectTimeOut' || 'Timeout:'
+            'path'           || 'Path:'
+            'sessionId'      || 'Session id:'
     }
-    
+
     def "get#field should return #value"() {
         expect:
             samplerGui."$field"
@@ -27,42 +32,50 @@ class WebsocketMessageSamplerGuiGuiSpec extends JmeterAbstractGuiSpec {
             'labelResource' || 'websocket.message.sampler.title'
             'staticLabel'   || 'Websocket Message Sampler'
     }
-    
+
     def "Should modify #property property with '#value' value from gui #component component"() {
         given:
-            WebsocketMessageSampler sampler = new WebsocketMessageSampler()
+            WebsocketSessionSampler sampler = new WebsocketSessionSampler()
             samplerGui."$component".text = value
         when:
             samplerGui.modifyTestElement(sampler)
         then:
             sampler."$property" == value
         where:
-            component   | property    | value
-            'message'   | 'message'   | 'Message'
-            'sessionId' | 'sessionId' | 'user1Session'
+            component        | property         | value
+            'serverNameOrIp' | 'serverNameOrIp' | '127.0.0.1'
+            'portNumber'     | 'portNumber'     | '8080'
+            'protocol'       | 'protocol'       | 'ws'
+            'connectTimeOut' | 'connectTimeOut' | '2000'
+            'path'           | 'path'           | 'websocket'
+            'sessionId'      | 'sessionId'      | 'user1Session'
     }
-    
+
     def "Should configure gui #component component with '#value' value from #property sampler property"() {
         given:
-            WebsocketMessageSampler sampler = new WebsocketMessageSampler()
+            WebsocketSessionSampler sampler = new WebsocketSessionSampler()
             sampler."$property" = value
         when:
             samplerGui.configure(sampler)
         then:
             samplerGui."$component".text == value
         where:
-            component   | property    | value
-            'message'   | 'message'   | 'Message'
-            'sessionId' | 'sessionId' | 'user1Session'
+            component        | property         | value
+            'serverNameOrIp' | 'serverNameOrIp' | '127.0.0.1'
+            'portNumber'     | 'portNumber'     | '8080'
+            'protocol'       | 'protocol'       | 'ws'
+            'connectTimeOut' | 'connectTimeOut' | '2000'
+            'path'           | 'path'           | 'websocket'
+            'sessionId'      | 'sessionId'      | 'user1Session'
     }
-    
+
     def "Should throw ClassCastException when testElement is not WebsocketMessageSampler on configure"() {
         when:
             samplerGui.configure(new HTTPSampler())
         then:
             thrown(ClassCastException)
     }
-    
+
     def "Should throw ClassCastException when testElement is not WebsocketMessageSampler on modifyTestElement"() {
         when:
             samplerGui.modifyTestElement(new HTTPSampler())
